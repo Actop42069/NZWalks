2d251eebe7b09e72c21a3b39fe5b53969f8a59ec
Achyut <agaihr10@gmail.com>
2023-12-11 13:40:00 +0545

    test:create a test api named student using GET

---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+
+	// https://localhost:portnumber/api/students
+	[Route("api/[controller]")]
+	[ApiController]
+	public class StudentsController : ControllerBase
+	{
+		// GET: https://localhost:portnumber/api/students
+		[HttpGet]
+		public IActionResult GetAllStudents()
+		{
+			string[] studentNames = new string[] { "Rabik", "Anamika", "Rebina", "Rohan" };
+			return Ok(studentNames);
+		}
+	}
+}
